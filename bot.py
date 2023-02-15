81efdfbe18658f153d03b6c5bf0f222bf3c5f898
leonvita91 <leonvita91@gmail.com>
2023-02-15 14:08:24 +0000

    add first bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os 
+import telebot
+teletok = os.environ.get('tokentele')
+
+bot = telebot.TeleBot(teletok)
+#massage handler
+text = 'Hi from pythoncode'
+@bot.message_handler(commands=['start','hello'])
+def send_welcome(message):
+    bot.reply_to(message, text)
+
+@bot.message_handler(func=lambda msg : True)
+def recive(message):
+    bot.reply_to(message, message.text)
+bot.infinity_polling()
