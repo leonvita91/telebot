24c9332ba732a7bd840e45572480895c7ce5f50d
leonvita91 <leonvita91@gmail.com>
2023-02-15 15:18:03 +0000

    fix message\

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ teletok = os.environ.get('tokentele')
 
 bot = telebot.TeleBot(teletok)
 #massage handler
-text = 'نحن مش حرفيين نحن مبرمجين و العالم يحلف برسنا'
+text = 'My bot is alive :)'
 @bot.message_handler(commands=['start','hello'])
 def send_welcome(message):
     bot.reply_to(message, text)
